25137be1ee6851213c5e9b7c532621a1e7485b26
PinedaAntonio <antonio.pinedatoro@iesvalleinclan.es>
2025-01-28 11:54:54 +0100

    hacer front tutorials

---
+++ React/Front Tutoriales React V1 2024/src/components/add-tutorial.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AddTutorial extends Component {
+    render() {
+        return (
+        <div>
+            <h2>Add Tutorial</h2>
+        </div>
+        );
+    }
+}
---
+++ React/Front Tutoriales React V1 2024/src/components/tutorial.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+//editar tutorial pero sin poder cambiar el id
+
+export default class Tutorial extends Component {
+    render() {
+        return (
+        <div>
+            <h2>Tutorial</h2>
+        </div>
+        );
+    }
+}
